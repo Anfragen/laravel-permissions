973594b573503f6aa51a29e235e46d6f91e5378c
João Pedro Lopes <joaopalopes24@gmail.com>
2022-11-25 19:36:23 -0300

    feat(lang): adding default system messages to a translation file

---
+++ lang/en/permissions.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Permission Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'block_roles' => ' Necessary roles are :roles',
+
+    'block_permissions' => ' Necessary permissions are :permissions',
+
+    'blocked' => 'User is not authorized to access this functionality.',
+
+];
